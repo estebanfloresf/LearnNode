db8a815d2083befe236084250eef728bfca34d06
Esteban <esteban_fs@hotmail.com>
2017-06-07 21:56:15 -0500

    Lessons Tags
    Signed-off-by: Esteban <esteban_fs@hotmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const {catchErrors} = require('../handlers/errorHandlers');
 
 // Do work here
@@ -23,5 +24,16 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStorebySlug));
 router.get('/tags', catchErrors(storeController.getStorebyTag));
 router.get('/tags/:tag', catchErrors(storeController.getStorebyTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+
+// 1) Need to validate the register form
+// 2) register the user
+// 3) we need to log them
+
+router.post('/register', userController.validateRegister);
+
 
 module.exports = router;
